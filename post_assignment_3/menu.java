9aa5ba936f09b7aa43ee7d013d265671b6ffd31a
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-16 12:04:31 +0530

    post_assignment updated

---
+++ post_assignment_3/menu.java
@@ -0,0 +1,85 @@
+package day_3;
+import java.util.Scanner;
+
+class node5 {
+    int data;
+    node5 next;
+
+    node5(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class menu {
+    node5 head;
+    public void insert(int data) {
+        node5 newNode = new node5(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            node5 temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+    public void delete() {
+        if (head == null) {
+            System.out.println("List is empty.");
+        } else {
+            head = head.next;
+        }
+    }
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty.");
+        } else {
+            node5 temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        menu list = new menu();
+        int choice;
+
+        do {
+            System.out.println("\n--- Menu ---");
+            System.out.println("1. Insert");
+            System.out.println("2. Delete");
+            System.out.println("3. Display");
+            System.out.println("4. Exit");
+            System.out.print("Enter choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter value to insert: ");
+                    int val = sc.nextInt();
+                    list.insert(val);
+                    break;
+                case 2:
+                    list.delete();
+                    break;
+                case 3:
+                    list.display();
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
+
+
+
---
+++ post_assignment_6/post.java
@@ -0,0 +1,74 @@
+package day_6;
+
+public class post {
+	private static class Node {
+
+        int val;
+
+        Node next;
+
+        Node(int v) { val = v; }
+
+    }
+
+    private Node head;
+
+
+
+    public void push(int val) {
+
+        Node n = new Node(val);
+
+        n.next = head;
+
+        head = n;
+
+    }
+
+    public int pop() {
+
+        if (head == null) throw new RuntimeException("Stack is empty");
+
+        int val = head.val;
+
+        head = head.next;
+
+        return val;
+
+    }
+
+    public int peek() {
+
+        if (head == null) throw new RuntimeException("Stack is empty");
+
+        return head.val;
+
+    }
+
+    public boolean isEmpty() {
+
+        return head == null;
+
+    }
+
+    public static void main(String[] args) {
+
+        post s = new post();
+
+        s.push(10);
+
+        s.push(20);
+
+        System.out.println(s.peek());
+
+        System.out.println(s.pop());
+
+        System.out.println(s.pop());
+
+        System.out.println(s.isEmpty());
+
+    }
+
+}
+
+
