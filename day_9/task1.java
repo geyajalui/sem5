d893dc0d7a67d0cab7e878fea93846bba7cd83c0
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-17 11:42:50 +0530

    day_9 updated

---
+++ day_9/task1.java
@@ -0,0 +1,45 @@
+package day_9;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class task1 {
+	// Lower Bound
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a[]= {4,2,35,5,3,12,77,24};
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+		System.out.println("Enter a Target: ");
+		int target=s.nextInt();
+		int left=0;
+		int right=a.length;
+		int mid;
+		
+		while(left<right) {
+			mid=(left+right)/2;
+			if(a[mid]<target) {
+				left=mid+1;
+			}
+			else if(a[mid]>=target) {
+				right=mid;
+			}
+		}
+		try {
+			
+		
+		if(((a[left])|(a[right]))>=target) {
+			System.out.println("\nLow Bound found at "+left+" and value "+a[left]);
+		}
+		else if (target>(a[left]|a[right])) {
+			System.out.println("\nOut of Bounds");
+		}}
+		catch(Exception e) {
+			System.out.println("Out of bounds, no low bounds....");
+		}
+		s.close();
+	}
+
+}
+
+
---
+++ day_9/task2.java
@@ -0,0 +1,37 @@
+package day_9;
+import java.util.Arrays;
+import java.util.Scanner;
+public class task2 {
+// Upper Bound
+
+		public static void main(String[] args) {
+			Scanner s=new Scanner(System.in);
+			int a[]= {4,2,35,5,3,12,77,24};
+			System.out.println(Arrays.toString(a));
+			System.out.println("Enter a Target: ");
+			int target=s.nextInt();
+			int left=0;
+			int right=a.length;
+			int mid;
+			Arrays.sort(a);
+			System.out.println("After Sorting \n"+Arrays.toString(a));
+			while(left<right) {
+				mid=(left+right)/2;
+				if(a[mid]<=target) {
+					left=mid+1;
+				}
+				else if(a[mid]>target) {
+					right=mid;
+				}
+			}
+			if(left<a.length) {
+				System.out.println("\nUpper Bound found at "+left+" and value "+a[left]);
+			}
+			else {
+				System.out.println("\nupper bound not found");
+			}
+			
+			s.close();
+		}
+
+	}
