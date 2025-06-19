07b30b05d5ac79b9b4133360102e6eafb2690dc4
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-19 12:23:36 +0530

    updated

--- day_10/task1.sql
+++ day_10/task1.sql
@@ -2,5 +2,5 @@ create database kgcas;
 use kgcas;
 create table student(name varchar(32),dept varchar(5),sec char(1), marks int);
 select * from student;
-insert into student values("pooja","ct","B",78),("yogapriya","ct","B",54),("sabthami","ct","A",80),("Harshini","ct","A",35),("Mithun","ct","B",25);
+insert into student values("riya","cs","B",78),("priya","cs","B",54),("sam","cs","b",80),("Harshini","cs","b",35),("Meera","ct","B",25);
 select * from student;
